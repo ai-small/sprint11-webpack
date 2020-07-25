a0c1530ff0221b2935dd2961e84fb972fe2ece42
ai-small <aismall@yandex.ru>
2020-07-25 15:03:33 +1200

    Modified webpack.config (setup image-webpack-loader)

--- webpack.config.js
+++ webpack.config.js
@@ -53,13 +53,37 @@ module.exports = {
       },
       {
         test: /\.(svg|jpg|jpeg|png|webp|gif|ico)/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: './images/[name].[ext]',
-            esModule: false
-          }
-        }
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './images/[name].[ext]',
+              esModule: false
+            }
+        },        
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },              
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },              
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
       }
     ]
   },
